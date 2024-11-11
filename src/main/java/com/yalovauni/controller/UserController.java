5e5ae46e04a4d395163c7a08ae43529f6ad2b443
aktashasan <aktashasan1130@gmail.com>
2024-11-11 14:59:30 +0300

    put, delete and getAll functions are enabled

--- src/main/java/com/yalovauni/controller/UserController.java
+++ src/main/java/com/yalovauni/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -27,9 +29,30 @@ public class UserController {
     }
 
     //findAll
-    //delete
-    //update
+    @GetMapping("/findAll")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
+    //delete
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    //update
+    @PutMapping("/update/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        existingUser.setUsername(user.getUsername());
+        existingUser.setPassword(user.getPassword());
+        userRepository.save(existingUser);
+        return new ResponseEntity<>(existingUser, HttpStatus.OK);
+    }
 
     @PostMapping("/add")
     public ResponseEntity<User> createUser(@RequestBody User user) {
